459c2a1487da4cfb1bdddac9f41d7c03201ceaa1
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-09-11 13:35:24 +0900

    Title: 정수 사각형 차이의 최소 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240911/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
+++ 240911/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
@@ -4,47 +4,40 @@ const stdin = fs.readFileSync(filePath).toString().split('\n');
 
 const input = (() => {
   let line = 0;
-  return () => stdin[line++];
+  return () => stdin[line += 1];
 })();
 
 const n = +input();
 const graph = Array.from(Array(n), () => input().split(" ").map(Number));
 const dp = Array.from(Array(n), () => Array(n).fill(null).map(() => ({min: Infinity, max: -Infinity})));
 
+function updateMinMax(current, newValue) {
+    return {
+        min: Math.min(current.min, newValue),
+        max: Math.max(current.max, newValue)
+    };
+}
+
 dp[0][0] = {min: graph[0][0], max: graph[0][0]};
 
-for (let j = 1; j < n; j += 1) {
-    dp[0][j] = {
-        min: Math.min(dp[0][j-1].min, graph[0][j]),
-        max: Math.max(dp[0][j-1].max, graph[0][j])
-    };
-}
+for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+        if (i === 0 && j === 0) continue;
 
-for (let i = 1; i < n; i += 1) {
-    dp[i][0] = {
-        min: Math.min(dp[i-1][0].min, graph[i][0]),
-        max: Math.max(dp[i-1][0].max, graph[i][0])
-    };
-}
+        let fromTop = i > 0 ? dp[i-1][j] : null;
+        let fromLeft = j > 0 ? dp[i][j-1] : null;
 
-for (let i = 1; i < n; i += 1) {
-    for (let j = 1; j < n; j += 1) {
-        const fromTop = dp[i-1][j];
-        const fromLeft = dp[i][j-1];
+        if (fromTop && fromLeft) {
+            let newFromTop = updateMinMax(fromTop, graph[i][j]);
+            let newFromLeft = updateMinMax(fromLeft, graph[i][j]);
+            let newDiffTop = newFromTop.max - newFromTop.min;
+            let newDiffLeft = newFromLeft.max - newFromLeft.min;
 
-        const diffTop = fromTop.max - fromTop.min;
-        const diffLeft = fromLeft.max - fromLeft.min;
-
-        if (diffTop <= diffLeft) {
-            dp[i][j] = {
-                min: Math.min(fromTop.min, graph[i][j]),
-                max: Math.max(fromTop.max, graph[i][j])
-            };
+            dp[i][j] = newDiffTop <= newDiffLeft ? newFromTop : newFromLeft;
+        } else if (fromTop) {
+            dp[i][j] = updateMinMax(fromTop, graph[i][j]);
         } else {
-            dp[i][j] = {
-                min: Math.min(fromLeft.min, graph[i][j]),
-                max: Math.max(fromLeft.max, graph[i][j])
-            };
+            dp[i][j] = updateMinMax(fromLeft, graph[i][j]);
         }
     }
 }
