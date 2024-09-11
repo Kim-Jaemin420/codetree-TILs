78f78b322cf7f08507ed788e5ba62dbf06b75656
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-09-11 13:36:32 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240911/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
+++ 240911/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : '백준알고리즘/input.txt';
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : '백준알고리즘/input.txt';
 const stdin = fs.readFileSync(filePath).toString().split('\n');
 
 const input = (() => {
   let line = 0;
-  return () => stdin[line += 1];
+  return () => stdin[line++];
 })();
 
 const n = +input();
