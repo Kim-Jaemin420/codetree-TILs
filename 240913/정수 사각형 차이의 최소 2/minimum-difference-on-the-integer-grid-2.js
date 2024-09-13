05f5b389b64c5656e27398bee8e4f9bcbee1ed36
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-09-13 12:53:24 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 245ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240913/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
+++ 240913/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '백준알고리즘/input.txt';
-const stdin = fs.readFileSync(filePath).toString().split('\n');
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const input = (() => {
-  let line = 0;
-  return () => stdin[line++];
-})();
+const n = parseInt(input[0]);
+const graph = input.slice(1).map(line => line.split(' ').map(Number));
 
-const n = +input();
-const graph = Array.from(Array(n), () => input().split(" ").map(Number));
+const dp = Array.from({length: n}, () => Array(n).fill(null));
 
-const dp = Array.from(Array(n), () => Array(n).fill(null).map(() => ({ min: Infinity, max: -Infinity })));
-
-dp[0][0] = { min: graph[0][0], max: graph[0][0] };
+dp[0][0] = [graph[0][0], graph[0][0]];
 
 for (let i = 0; i < n; i += 1) {
-  for (let j = 0; j < n; j += 1) {
-    if (i === 0 && j === 0) continue;
+    for (let j = 0; j < n; j += 1) {
+        if (i === 0 && j === 0) continue;
 
-    const current = graph[i][j];
-    let fromTop = i > 0 ? dp[i-1][j] : null;
-    let fromLeft = j > 0 ? dp[i][j-1] : null;
+        const current = graph[i][j];
+        let fromTop = i > 0 ? dp[i-1][j] : null;
+        let fromLeft = j > 0 ? dp[i][j-1] : null;
 
-    if (fromTop && fromLeft) {
-      dp[i][j].min = Math.min(Math.min(fromTop.min, fromLeft.min), current);
-      dp[i][j].max = Math.max(Math.max(fromTop.max, fromLeft.max), current);
-    } else if (fromTop) {
-      dp[i][j].min = Math.min(fromTop.min, current);
-      dp[i][j].max = Math.max(fromTop.max, current);
-    } else {
-      dp[i][j].min = Math.min(fromLeft.min, current);
-      dp[i][j].max = Math.max(fromLeft.max, current);
+        if (fromTop && fromLeft) {
+            const diffTop = Math.max(fromTop[1], current) - Math.min(fromTop[0], current);
+            const diffLeft = Math.max(fromLeft[1], current) - Math.min(fromLeft[0], current);
+            
+            if (diffTop <= diffLeft) {
+                dp[i][j] = [Math.min(fromTop[0], current), Math.max(fromTop[1], current)];
+            } else {
+                dp[i][j] = [Math.min(fromLeft[0], current), Math.max(fromLeft[1], current)];
+            }
+        } else if (fromTop) {
+            dp[i][j] = [Math.min(fromTop[0], current), Math.max(fromTop[1], current)];
+        } else {
+            dp[i][j] = [Math.min(fromLeft[0], current), Math.max(fromLeft[1], current)];
+        }
     }
-  }
 }
 
-console.log(dp[n-1][n-1].max - dp[n-1][n-1].min);
+console.log(dp[n-1][n-1][1] - dp[n-1][n-1][0]);
